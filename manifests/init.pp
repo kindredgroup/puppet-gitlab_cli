221e46e519c1216350669501c7b0a8f101627e58
Ilja Bobkevic <ilja.bobkevic@unibet.com>
2014-11-18 15:10:25 +0100

    Simplify...

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,9 +35,20 @@
 #
 # Copyright 2014 North Development AB
 #
-class gitlab_cli {
+class gitlab_cli(
+  $path           = $::gitlab_cli::params::path,
+  $owner          = $::gitlab_cli::params::owner,
+  $url            = $::gitlab_cli::params::url,
+  $private_token  = $::gitlab_cli::params::private_token,
+  $ssl_verify     = $::gitlab_cli::params::ssl_verify,
+  $timeout        = $::gitlab_cli::params::timeout
+) inherits gitlab_cli::params {
 
-  class { 'gitlab_cli::package': } ->
-  class { 'gitlab_cli::config': }
+  python::pip { 'python-gitlab' : } ->
+  file { "$path/.python-gitlab.cfg":
+    owner => $owner,
+    mode  => 0400,
+    content => template("${module_name}/python-gitlab.cfg.erb"),
+  }
 
 }
